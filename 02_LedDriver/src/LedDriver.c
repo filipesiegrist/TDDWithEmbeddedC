d27fcc8dc55d854be91d448b25093c62ad6334b5
filipesiegrist <filipebrasil8@gmail.com>
2026-08-09 15:13:57 -0300

    extracted updateHardware static function

--- 02_LedDriver/src/LedDriver.c
+++ 02_LedDriver/src/LedDriver.c
@@ -4,32 +4,37 @@ static uint16_t* ledsAddress;
 static uint16_t ledsImage;
 
 static uint16_t convertLedNumberToBit(int ledNumber);
+static void updateHardware(void);
 
 // =============== Public methods =================
 
 void LedDriver_Create(uint16_t* address) {
     ledsAddress = address;
     ledsImage = ALL_LEDS_OFF;
-    *ledsAddress = ledsImage;
+    updateHardware();
 }
 
 void LedDriver_TurnOn(int ledNumber) {
     ledsImage |= convertLedNumberToBit(ledNumber);
-    *ledsAddress = ledsImage;
+    updateHardware();
 }
 
 void LedDriver_TurnOff(int ledNumber) {
     ledsImage &= ~convertLedNumberToBit(ledNumber);
-    *ledsAddress = ledsImage;
+    updateHardware();
 }
 
 void LedDriver_TurnAllOn(void) {
     ledsImage = ALL_LEDS_ON;
-    *ledsAddress = ledsImage;
+    updateHardware();
 }
 
 // =============== Private methods =================
 
 static uint16_t convertLedNumberToBit(int ledNumber) {
     return (1 << (ledNumber - 1));
+}
+
+static void updateHardware(void) {
+    *ledsAddress = ledsImage;
 }
