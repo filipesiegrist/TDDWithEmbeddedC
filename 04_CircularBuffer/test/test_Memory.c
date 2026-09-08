5b38586032edac99ed9949b892bd2009f4e2a718
filipesiegrist <filipebrasil8@gmail.com>
2026-09-07 22:26:30 -0300

    FakeMemory ideas added along with a test file for this fake module

---
+++ 04_CircularBuffer/test/test_Memory.c
@@ -0,0 +1,21 @@
+
+#ifdef TEST
+
+#include "unity.h"
+
+#include "Memory.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_Memory_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement Memory");
+}
+
+#endif // TEST
